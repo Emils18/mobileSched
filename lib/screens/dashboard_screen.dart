ace59ecd20bebac706b16d8796dedca18d4de2f9
Mio Monda <emeliomondares14@gmail.com>
2026-08-23 00:47:40 +0800

    Fix activeColor parameter for latest Flutter version

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1552,7 +1552,7 @@ class _SettingsSheetContentState extends State<_SettingsSheetContent> {
       child: SwitchListTile(
         contentPadding: EdgeInsets.zero,
         value: value,
-        activeThumbColor: colorScheme.primary,
+        activeColor: colorScheme.primary,
         onChanged: enabled
             ? (newValue) {
                 onChanged(newValue);
